3d7d922006c03e17098166f1117ce8cbe46a8794
Eddy <edwardyue93@gmail.com>
2023-05-19 21:57:55 +0200

    Created label element

--- dinos/ui/fps_stats.py
+++ dinos/ui/fps_stats.py
@@ -2,6 +2,7 @@ from dinos.common.game_abstract import Renderable, Updatable
 from dinos.config import Config
 from dinos.resources.font_manager import FontManager
 from dinos.states.state import StateTypes
+from dinos.ui.label import UILabel
 
 
 class FPSStats(Updatable, Renderable):
@@ -10,7 +11,20 @@ class FPSStats(Updatable, Renderable):
         self.__logic_frames = 0
         self.__render_frames = 0
         self.__update_time = 0
-        self.__set_fps_surface()
+
+        font = FontManager.instance().get(
+            StateTypes.Global,
+            Config.get("fonts", "main", "name")
+        )
+
+        self.__label = UILabel(
+            font,
+            "0 - 0",
+            Config.get("timing", "fps_pos"),
+            Config.get("fonts", "main", "color"),
+            Config.get("game", "background_color")
+        )
+
         self.__refresh_update_time = Config.get(
             "timing", "refresh_update_time")
 
@@ -19,20 +33,19 @@ class FPSStats(Updatable, Renderable):
         self.__logic_frames += 1
 
         if self.__update_time > self.__refresh_update_time:
-            self.__set_fps_surface()
+            self.__set_text()
             self.__logic_frames = 0
             self.__render_frames = 0
             self.__update_time -= self.__refresh_update_time
 
-    def render(self, surface_dst):
+    def render(self, surface):
         self.__render_frames += 1
-        surface_dst.blit(self.__image, Config.get("timing", "fps_pos"))
+        self.__label.render(surface)
 
     def quit(self):
         pass
 
-    def __set_fps_surface(self):
-        font = FontManager.instance().get(
-            StateTypes.Global, Config.get("fonts", "main", "name"))
-        self.__image = font.render(
-            f"{self.__logic_frames} - {self.__render_frames}", True, Config.get("fonts", "main", "color"), None)
+    def __set_text(self):
+        self.__label.set_text(
+            f"{self.__logic_frames} - {self.__render_frames}"
+        )
---
+++ dinos/ui/label.py
@@ -0,0 +1,41 @@
+from dinos.common.game_abstract import Renderable
+
+
+class UILabel(Renderable):
+
+    def __init__(self, font, text, position, color, bg_color):
+        self.__font = font
+        self.__text = text
+        self.__color = color
+        self.__position = position
+        self.__bg_color = bg_color
+        self.__set_image()
+
+    def set_text(self, text):
+        if text == self.__text:
+            return
+        self.__text = text
+        self.__set_image()
+
+    def set_color(self, color):
+        if color == self.__color:
+            return
+        self.__color = color
+        self.__set_image()
+
+    def set_position(self, position):
+        if position == self.__position:
+            return
+        self.__position = position
+        self.__set_image()
+
+    def render(self, surface):
+        surface.blit(self.__image, self.__rect)
+
+    def quit(self):
+        pass
+
+    def __set_image(self):
+        self.__image = self.__font.render(
+            self.__text, True, self.__color, self.__bg_color)
+        self.__rect = self.__image.get_rect(center=self.__position)
