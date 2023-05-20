5a0396bb04f26b6dd064124d6d645c4d9ce2c501
Eddy <edwardyue93@gmail.com>
2023-05-20 20:38:12 +0200

    Allow static animations

--- dinos/resources/animation.py
+++ dinos/resources/animation.py
@@ -3,18 +3,24 @@ from dinos.common.game_abstract import Renderable, Updatable
 
 class Animation(Updatable):
     def __init__(self, sequence, duration):
+        self.__duration = duration
         self.__sequence = sequence
         self.__total_sequence = len(sequence)
-        self.__time_per_frame = duration / len(sequence)
         self.__current_frame = 0
         self.__last_frame_time = 0
         self.__current_sequence = sequence[0]
+
+        if duration != None:
+            self.__time_per_frame = duration / len(sequence)
 
     def restart(self):
         self.__current_frame = 0
         self.__last_frame_time = 0
 
     def update(self, delta_time):
+        if self.__duration == None:
+            return
+
         if self.__last_frame_time > self.__time_per_frame:
             self.__current_frame = self.__current_frame + 1
             if self.__current_frame >= self.__total_sequence:
